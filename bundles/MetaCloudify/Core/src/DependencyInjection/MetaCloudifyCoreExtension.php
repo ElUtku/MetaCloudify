449c33c10cb49afd13bb91ed7c86646a07cd922b
ElUtku <masandres2000@gmail.com>
2024-04-19 17:39:47 +0200

    DependencyInjection fix path

--- bundles/MetaCloudify/Core/src/DependencyInjection/MetaCloudifyCoreExtension.php
+++ bundles/MetaCloudify/Core/src/DependencyInjection/MetaCloudifyCoreExtension.php
@@ -15,7 +15,7 @@ class MetaCloudifyCoreExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
     }
 }
--- bundles/MetaCloudify/Ftp/src/DependencyInjection/MetaCloudifyFtpExtension.php
+++ bundles/MetaCloudify/Ftp/src/DependencyInjection/MetaCloudifyFtpExtension.php
@@ -15,7 +15,7 @@ class MetaCloudifyFtpExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'../Resources/config'));
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
         $loader->load('services.yaml');
     }
 }
