06b99739d621a95b4a75381fe7d5cc7b14e51d81
ElUtku <masandres2000@gmail.com>
2024-04-19 17:33:03 +0200

    DependencyInjection

---
+++ bundles/MetaCloudify/Home/src/DependencyInjection/MetaCloudifyHomeExtension.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace MetaCloudify\Home\DependencyInjection;
+
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\XmlFileLoader;
+
+class MetaCloudifyHomeExtension extends Extension
+{
+
+    /**
+     * @inheritDoc
+     */
+    public function load(array $configs, ContainerBuilder $container)
+    {
+        $loader = new XmlFileLoader($container, new FileLocator(__DIR__.'../Resources/config'));
+        $loader->load('services.yaml');
+    }
+}
