d83163fad5352397b1eaf1e8a9d22166f1c89838
ElUtku <masandres2000@gmail.com>
2024-05-05 20:31:46 +0200

    custom script

--- bundles/MetaCloudify/OwnCloudBundle/src/Resources/config/CustomInstallerScript.php
+++ bundles/MetaCloudify/OwnCloudBundle/src/Resources/config/CustomInstallerScript.php
@@ -1,16 +1,15 @@
 <?php
-namespace MetaCloudify\CoreBundle\Resources\config;
+namespace MetaCloudify\OwnCloudBundle\Resources\config;
 
 class CustomInstallerScript
 {
     public static function run(): void
     {
         $rootDir = dirname(__DIR__, 6);
-
         $file = $rootDir . '/config/routes/annotations.yaml';
         $lineToAdd = "\n" .
             "\nmetacloudify_corebundle:" .
-            "\n    resource: '@MetaCloudifyCoreBundle/src/Resources/config/annotations.yaml'" .
+            "\n    resource: '@MetaCloudifyOwnCloudBundle/src/Resources/config/annotations.yaml'" .
             "\n";
 
         file_put_contents($file, $lineToAdd, FILE_APPEND);
